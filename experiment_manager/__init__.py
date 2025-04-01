d3633a796ec4295c8330f7ae3eb8a97aae0243c4
roymicha3 <roymicha3@gmail.com>
2025-04-01 19:05:24 +0300

    initial commit

---
+++ experiment_manager/__init__.py
@@ -0,0 +1,5 @@
+"""
+Experiment Manager package initialization.
+"""
+
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="experiment_manager",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here
+    ],
+    author="Roy Michael",
+    description="A Python package for managing experiments",
+    python_requires=">=3.6",
+)
