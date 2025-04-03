b1ab83a6e9ca4090a7070a7f9e1f58b3dea559d1
roymicha3 <roymicha3@gmail.com>
2025-04-03 16:06:04 +0300

    added callback factory and checkpoint callback

---
+++ experiment_manager/pipelines/callbacks/checkpoint.py
@@ -0,0 +1,51 @@
+import os
+from typing import Dict, Any
+from omegaconf import DictConfig
+
+from experiment_manager.environment import Environment
+from experiment_manager.pipelines.callbacks.callback import Callback, Metric
+from experiment_manager.common.serializable import YAMLSerializable
+
+
+@YAMLSerializable.register("CheckpointCallback")
+class CheckpointCallback(Callback, YAMLSerializable):
+    """
+    Metrics tracker for storing training statistics
+    """
+    CHECKPOINT_NAME = "checkpoint"
+    
+    def __init__(self, interval: int, env: Environment):
+        super(CheckpointCallback, self).__init__()
+        super(YAMLSerializable, self).__init__()
+        
+        self.index = 0
+        self.current_checkpoint = 0
+        
+        self.interval = interval
+        self.env = env
+        
+        self.checkpoint_path = os.path.join(self.env.artifact_dir, CheckpointCallback.CHECKPOINT_NAME)
+        
+    def on_epoch_end(self, epoch_idx, metrics: Dict[str, Any]) -> bool:
+        """Called at the end of each epoch."""
+        self.index += 1
+        if self.index % self.interval == 0:
+            file_path = f"{self.checkpoint_path}-{self.parent_id}-{self.current_checkpoint}"
+            metrics[Metric.NETWORK].save(file_path)
+            self.current_checkpoint += 1
+        
+        return True
+
+    def on_end(self, metrics: Dict[str, Any]):
+        """Called at the end of training."""
+        pass
+
+    def get_latest(self, key: str, default: Any = None) -> Any:
+        pass
+    
+    @classmethod
+    def from_config(cls, config: DictConfig, env: Environment):
+        """
+        Create an instance from a DictConfig.
+        """
+        return cls(config.interval, env)
